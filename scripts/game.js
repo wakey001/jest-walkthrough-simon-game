a081a6996c43983aca07acd83f523a367750bfd3
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-05-07 13:35:13 +0000

    updated mistake line 13 game.js missing code

--- scripts/game.js
+++ scripts/game.js
@@ -10,6 +10,17 @@ function newGame() {
     game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
 }
@@ -42,4 +53,17 @@ function showTurns() {
     }, 800);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]){
+        if (game.currentGame.length == game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn()
+        }
+    }else{
+        alert("Wrong move!");
+        newGame();
+    }
+}
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,8 +2,9 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn, showTurns  } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn  } = require("../game");
 
+jest.spyOn(window, "alert").mockImplementation(() => {});
 beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
@@ -55,6 +56,12 @@ describe("newGame works correctly", () => {
     test("should display 0 for the element with the id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
+    test("expect data-listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
+    });
 
 });
 
@@ -87,5 +94,18 @@ describe("gameplay works correctly", () => {
         expect(game.turnNumber).toBe(0);
 
     });
+    test("should increment the score if the turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    });
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong move!");    
+    });
 
 });
+
+
+
